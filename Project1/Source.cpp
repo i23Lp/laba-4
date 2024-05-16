c6e42a359ffbe16257e4f915b4d69da383bbc7e8
i23Lp <djll1tr1y@gmail.com>
2024-05-16 12:37:38 +0300

    5логика движения и отскока

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -14,5 +14,17 @@ int main()
     }
     polygon1.setFillColor(sf::Color::Green);
     polygon1.setPosition(window1.getSize().x / 4.f, window1.getSize().y / 2.f);
-    sf::Vector2f velocity1(5.f, 5.f);
+
+    sf::Vector2f velocity1(5.f, 5.f); // Скорость полигона
+    bool moveRight = true; // Направление движения
+
+    while (window1.isOpen())
+    {
+        sf::Event event;
+        while (window1.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window1.close();
+        }
+    }
 }
