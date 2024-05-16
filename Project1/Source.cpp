7c1cebe81cafab32a11b2e58f17cc3f420961ba1
MyName3axap <3axapxojlonyk@gmail.com>
2024-05-16 14:26:17 +0300

    20 изменение скорости фигур

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -14,8 +14,8 @@ int main()
     circle.setPosition(window1.getSize().x / 2.f, window1.getSize().y / 4.f);
     circle.setFillColor(Color::Yellow);
 
-    Vector2f velocity1(2.5f, 2.5f);
-    Vector2f velocity2(-2.5f, -2.5f);
+    Vector2f velocity1(0.05f, 0.05f);
+    Vector2f velocity2(-0.05f, -0.05f);
     bool moveRight = true;
 
     // Установка цвета прямоугольника в желтый
