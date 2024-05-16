304d55adb97a03f142c7cd4f0acf79d8d1607eb1
MyName3axap <3axapxojlonyk@gmail.com>
2024-05-16 14:36:51 +0300

    28 удаление std: : и sf: :

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,6 +1,7 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/System.hpp>
 #include <random>
+
 using namespace sf;
 using namespace std;
 
@@ -8,23 +9,20 @@ int main()
 {
     RenderWindow window1(VideoMode(800, 600), "Многоугольники");
 
-    
-    sf::RectangleShape rectangle(sf::Vector2f(100, 50));  
+    RectangleShape rectangle(sf::Vector2f(100, 50));
     rectangle.setPosition(window1.getSize().x / 4.f, window1.getSize().y / 2.f);
     rectangle.setFillColor(Color::Yellow);
 
-    
-    sf::CircleShape circle(50);  
+    CircleShape circle(50);
     circle.setPosition(window1.getSize().x / 2.f, window1.getSize().y / 4.f);
     circle.setFillColor(Color::Red);
 
-    Vector2f velocity1(2.5f, 2.5f);  
-    Vector2f velocity2(-2.5f, -2.5f);  
+    Vector2f velocity1(2.5f, 2.5f);
+    Vector2f velocity2(-2.5f, -2.5f);
     bool moveRight = true;
 
-    
-    std::default_random_engine generator;
-    std::uniform_int_distribution<int> distribution(1, 2);
+    default_random_engine generator;
+    uniform_int_distribution<int> distribution(1, 2);
 
     while (window1.isOpen())
     {
@@ -35,7 +33,6 @@ int main()
                 window1.close();
         }
 
-       
         rectangle.move(velocity1.x, velocity1.y);
         if (moveRight && rectangle.getPosition().x + rectangle.getSize().x > window1.getSize().x)
         {
@@ -53,12 +50,9 @@ int main()
             velocity1.y *= -1;
         }
 
-      
         circle.move(velocity2.x, velocity2.y);
 
-         
         if (rectangle.getGlobalBounds().intersects(circle.getGlobalBounds())) {
-           
             if (distribution(generator) == 1) {
                 velocity1.x = -velocity1.x;
             }
@@ -67,10 +61,9 @@ int main()
             }
         }
 
-        
         window1.clear();
         window1.draw(rectangle);
-        window1.draw(circle); 
+        window1.draw(circle);
         window1.display();
     }
 
