c23d4b5bfeed07d59a9b0f33c04ab0c4c34c08d6
i23Lp <djll1tr1y@gmail.com>
2024-05-16 12:33:43 +0300

    4 присваивание цвета

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -12,5 +12,7 @@ int main()
         float angle = 2.f * 3.14 * i / sides1 - 3.14 / 2.f;
         polygon1.setPoint(i, sf::Vector2f(cos(angle) * radius1, sin(angle) * radius1));
     }
-
+    polygon1.setFillColor(sf::Color::Green);
+    polygon1.setPosition(window1.getSize().x / 4.f, window1.getSize().y / 2.f);
+    sf::Vector2f velocity1(5.f, 5.f);
 }
