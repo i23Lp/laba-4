0a78354247cfef56cd380e6d6142fdd3f50bbc0d
i23Lp <djll1tr1y@gmail.com>
2024-05-16 12:32:04 +0300

    3 создание многоугольника

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -3,6 +3,14 @@
 int main()
 {
     sf::RenderWindow window1(sf::VideoMode(800, 600), "Многоугольники");
-
+    int sides1 = 5;
+    float radius1 = 50.f;
+    sf::ConvexShape polygon1;
+    polygon1.setPointCount(sides1);
+    for (int i = 0; i < sides1; i++)
+    {
+        float angle = 2.f * 3.14 * i / sides1 - 3.14 / 2.f;
+        polygon1.setPoint(i, sf::Vector2f(cos(angle) * radius1, sin(angle) * radius1));
+    }
 
 }
