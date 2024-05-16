df6c628e3cc3260bc3d96db50daec996dd8f95f4
MyName3axap <3axapxojlonyk@gmail.com>
2024-05-16 14:33:25 +0300

    25 логика изменения скорости при ударе о ребро окна

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,23 +1,29 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/System.hpp>
+#include <random>
 using namespace sf;
 
 int main()
 {
-    RenderWindow window1(VideoMode(800, 600), L"фигуры");
+    RenderWindow window1(VideoMode(800, 600), "Многоугольники");
 
-   
-    sf::RectangleShape rectangle(sf::Vector2f(100, 50)); 
+    // Создание прямоугольника
+    sf::RectangleShape rectangle(sf::Vector2f(100, 50)); // Размер прямоугольника
     rectangle.setPosition(window1.getSize().x / 4.f, window1.getSize().y / 2.f);
+    rectangle.setFillColor(Color::Yellow);
 
-    sf::CircleShape circle(50);  
+    // Создание круга
+    sf::CircleShape circle(50); // Радиус круга
     circle.setPosition(window1.getSize().x / 2.f, window1.getSize().y / 4.f);
-    circle.setFillColor(Color::Yellow);
+    circle.setFillColor(Color::Red);
 
-    Vector2f velocity1(0.05f, 0.05f);
-    Vector2f velocity2(-0.05f, -0.05f);
+    Vector2f velocity1(2.5f, 2.5f); // Уменьшенная скорость
+    Vector2f velocity2(-2.5f, -2.5f); // Скорость круга
     bool moveRight = true;
 
+    // Генератор случайных чисел
+    std::default_random_engine generator;
+    std::uniform_int_distribution<int> distribution(1, 2);
 
     while (window1.isOpen())
     {
@@ -28,6 +34,7 @@ int main()
                 window1.close();
         }
 
+        // Обновление положения прямоугольника
         rectangle.move(velocity1.x, velocity1.y);
         if (moveRight && rectangle.getPosition().x + rectangle.getSize().x > window1.getSize().x)
         {
@@ -44,18 +51,25 @@ int main()
         {
             velocity1.y *= -1;
         }
+
+        // Обновление положения круга
         circle.move(velocity2.x, velocity2.y);
+
+        // Проверка столкновения и изменение скорости
         if (rectangle.getGlobalBounds().intersects(circle.getGlobalBounds())) {
-         
-            velocity1.x = -velocity1.x;
-            velocity1.y = -velocity1.y;
-            velocity2.x = -velocity2.x;
-            velocity2.y = -velocity2.y;
+            // Случайное изменение направления одного из объектов
+            if (distribution(generator) == 1) {
+                velocity1.x = -velocity1.x;
+            }
+            else {
+                velocity2.x = -velocity2.x;
+            }
         }
- 
+
+        // Отрисовка
         window1.clear();
         window1.draw(rectangle);
-        window1.draw(circle);
+        window1.draw(circle); // Рисуем круг
         window1.display();
     }
 
